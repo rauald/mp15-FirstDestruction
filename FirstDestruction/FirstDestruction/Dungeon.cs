6f6f5e12d53906bfd6fec7003a903b36196dc11f
pkoj0328@gmail.com <pkoj0328@gmail.com>
2026-08-27 14:41:14 +0900

    던전 클래스 수정

--- FirstDestruction/FirstDestruction/Dungeon.cs
+++ FirstDestruction/FirstDestruction/Dungeon.cs
@@ -2,5 +2,13 @@
 
 public class Dungeon
 {
-    string name;    
+    string name;
+    string description;
+    public Dungeon(string name, string description)
+    {
+        this.name = name;
+        this.description = description;
+    }
+    public string Name { get { return name; } }
+    public string Description { get { return description; } }
 }
