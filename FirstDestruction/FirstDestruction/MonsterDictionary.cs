c836abd4acd7800a494cf492217acf276fbb1a1b
lemoncube12@gmail.com <lemoncube12@gmail.com>
2026-08-27 14:54:39 +0900

    3개씩추가

---
+++ FirstDestruction/FirstDestruction/MonsterDictionary.cs
@@ -0,0 +1,29 @@
+﻿
+
+class MonsterDictionary
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Hello Destruction!");
+
+        string[] monsterNames = { "파이리", "꼬부기", "이상해씨" };
+        string[] monsterTypes = { "불", "물", "풀" };
+        int[] monsterHp = { 140, 180, 160 };
+
+
+        for (int i = 0; i < monsterNames.Length; i++)
+        {
+            PrintMonster(i, monsterNames[i], monsterTypes[i], monsterHp[i]);
+        }
+
+
+
+
+        static void PrintMonster(int number, string name, string type, int hp)
+        {
+            Console.WriteLine($"{number}번 {name} ({type})  체력 {hp}");
+        }
+
+
+    }
+}
