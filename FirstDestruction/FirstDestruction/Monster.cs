9cd7f61952f34ee82dbf293497b2a944b3c1c452
soundmusic64@gmail.com <soundmusic64@gmail.com>
2026-08-27 14:36:35 +0900

    [Add] Monster State

--- FirstDestruction/FirstDestruction/Monster.cs
+++ FirstDestruction/FirstDestruction/Monster.cs
@@ -1,4 +1,23 @@
 ﻿public class Monster
 {
-    public int Health { get; set; }
+    private string _name;
+    public string Name { get { return _name; } protected set { _name = value} }
+
+    private int _health;
+
+    public int Health { get { return _health; } protected set { _health = value; } }
+
+    private int _atk;
+    public int Atk { get { return _atk; } protected set { _atk = value; } }
+
+    private bool _isDie;
+    public bool IsDie { get { return _isDie; } protected set { _isDie = value; } }
+
+    public Monster(string name, int health)
+    {
+        _name = name;
+        _health = health;
+        IsDie = false;
+    }
+    
 }
